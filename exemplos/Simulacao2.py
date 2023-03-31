d6c35a054ed8f0ef7fb9952a26988de03df5a41d
rafa rrayes <52227000+rrayes3110@users.noreply.github.com>
2023-03-31 15:51:08 -0300

    Update Simulacao2.py

--- exemplos/Simulacao2.py
+++ exemplos/Simulacao2.py
@@ -1,25 +1,25 @@
-import pygame
-import MiniEngine
+from Miniengine import *
+#setup a simulation
+sim = Simulation(400, 800, "Simulation", True, 0.001, 60000, 2)
+
+#add objects
+ball = Object(20, 20, (200, 200), 2, 0, 0, "yellow", sim, hitBorder=True, circle=True, collisionLayer=0)
+ball2 = Object(20, 20, (215, 185), 2, 0, 0, "red", sim, hitBorder=True, circle=True)
+ball3 = Object(20, 20, (185, 185), 2, 0, 0, "green", sim, hitBorder=True, circle=True)
+ball4 = Object(20, 20, (200, 170), 2, 0, 0, "blue", sim, hitBorder=True, circle=True)
+ball5= Object(20, 20, (230, 170), 2, 0, 0, "black", sim, hitBorder=True, circle=True)
+ball6 = Object(20, 20, (170, 170), 2, 0, 0, "orange", sim, hitBorder=True, circle=True)
+ball7 = Object(20, 20, (200, 600), 3, 200, -90, "white", sim, hitBorder=True, circle=True, collisionLayer=0)
 
 
+sim.add_object(ball)
+sim.add_object(ball2)
+sim.add_object(ball3)
+sim.add_object(ball4)
+sim.add_object(ball5)
+sim.add_object(ball6)
+sim.add_object(ball7)
 
-tela = MiniEngine.Tela(800, 800, "Simulação", tick=60, speed=1)
-terra = MiniEngine.Objeto(20, 20, (400, 400), 3, 0, 0, "verde", tela)
-sol = MiniEngine.Objeto(50, 50, (400, 500), 20, 20, -90, "amarelo", tela)
-Jogando = True
-clock = pygame.time.Clock()
-tick = 60
-dt = 1/tick
-while Jogando:
-    tela.tela.fill((0,0,0))
-    sol.atualizarPos(dt)
-    terra.atualizarPos(dt, gravidade=True)
-    tela.desenhar(sol)
-    tela.desenhar(terra)
-    clock.tick(tick)
-    if terra.ChecarColidir(sol):
-        MiniEngine.Colide(sol, terra, COR=1)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Jogando = False
-    pygame.display.update()
+#calculate with initial conditions
+sim.calculate()
+sim.run()
