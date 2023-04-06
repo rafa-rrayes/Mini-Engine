302b0b2aa1baf8342b1d0b8234311d65afe7b0f8
rafa rrayes <52227000+rrayes3110@users.noreply.github.com>
2023-04-06 20:16:39 -0300

    Update Simulacao2.py

--- exemplos/Simulacao2.py
+++ exemplos/Simulacao2.py
@@ -1,25 +1,16 @@
-from Miniengine import *
-#setup a simulation
-sim = Simulation(400, 800, "Simulation", True, 0.001, 60000, 2)
-
-#add objects
-ball = Object(20, 20, (200, 200), 2, 0, 0, "yellow", sim, hitBorder=True, circle=True, collisionLayer=0)
-ball2 = Object(20, 20, (215, 185), 2, 0, 0, "red", sim, hitBorder=True, circle=True)
-ball3 = Object(20, 20, (185, 185), 2, 0, 0, "green", sim, hitBorder=True, circle=True)
-ball4 = Object(20, 20, (200, 170), 2, 0, 0, "blue", sim, hitBorder=True, circle=True)
-ball5= Object(20, 20, (230, 170), 2, 0, 0, "black", sim, hitBorder=True, circle=True)
-ball6 = Object(20, 20, (170, 170), 2, 0, 0, "orange", sim, hitBorder=True, circle=True)
-ball7 = Object(20, 20, (200, 600), 3, 200, -90, "white", sim, hitBorder=True, circle=True, collisionLayer=0)
+from JustEngine import *
 
 
-sim.add_object(ball)
-sim.add_object(ball2)
-sim.add_object(ball3)
-sim.add_object(ball4)
-sim.add_object(ball5)
-sim.add_object(ball6)
-sim.add_object(ball7)
+sim = Simulation(400, 800, "Titulooo", True)
 
-#calculate with initial conditions
-sim.calculate()
-sim.run()
+
+ball = Object(20, 20, (200, 200), 2, 0, 0, "yellow", sim, hitBorder=True, circle=True, COR = 1)
+ball2 = Object(20, 20, (215, 185), 2, 0, 0, "red", sim, hitBorder=True, circle=True, COR = 1)
+ball3 = Object(20, 20, (185, 185), 2, 0, 0, "green", sim, hitBorder=True, circle=True, COR = 1) 
+ball4 = Object(20, 20, (200, 170), 2, 0, 0, "blue", sim, hitBorder=True, circle=True, COR = 1)
+ball5= Object(20, 20, (230, 170), 2, 0, 0, "black", sim, hitBorder=True, circle=True, COR = 1)
+ball6 = Object(20, 20, (170, 170), 2, 0, 0, "orange", sim, hitBorder=True, circle=True, COR = 1)
+ball7 = Object(20, 20, (200, 600), 10, 200, 270, "white", sim, hitBorder=True, circle=True, COR = 1)
+
+sim.calculate(20, 0.001)
+sim.run(speed=2)
