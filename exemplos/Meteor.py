74c900cc7b65ebe9243f0346d4c2f2eb051a2b43
rafa rrayes <52227000+rrayes3110@users.noreply.github.com>
2023-04-06 22:24:25 -0300

    Update Meteor.py

--- exemplos/Meteor.py
+++ exemplos/Meteor.py
@@ -1,28 +1,13 @@
-import pygame
-import MiniEngine
+import Miniengine
 
-tela = MiniEngine.Window(800, 800, "Simulação")
-terra = MiniEngine.Object(20, 20, (400, 600), 30, 40, -180, (0,0,255), tela)
-sol = MiniEngine.Object(50, 50, (400, 400), 20, 0, 0, (255, 255, 0), tela)
-asteroide = MiniEngine.Object(10, 10, (-770, -770), 20, 40, 45, "grey", tela)
+tela = Miniengine.Simulation(800, 800, "Simulação","black", True)
 
-Jogando = True
-clock = pygame.time.Clock()
-tick = 60
-dt = dt = (1/tick)*4
-while Jogando:
-    tela.window.fill((0,0,0))
-    terra.gravitate(sol,True, dt, g=10)
-    terra.updatePosition(dt)
-    sol.updatePosition(dt)
-    tela.draw(sol)
-    tela.draw(terra)
-    asteroide.updatePosition(dt)
-    tela.draw(asteroide)
-    clock.tick(tick)
-    if terra.checkColision(asteroide):
-        MiniEngine.Colide(asteroide, terra, COR=1)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Jogando = False
-    pygame.display.update()
+terra = Miniengine.Object(20, 20, (400, 600), 10, 150, -180, "blue", tela,  circle=True)
+sol = Miniengine.Object(50, 50, (400, 400), 400, 0, 0, "yellow", tela,  circle=True)
+asteroide = Miniengine.Object(10, 10, (-3500, 600), 1, 190, 0, "grey", tela, circle=True)
+
+sol.addGravity(10)
+sol.lockPos
+terra.addGravity(10)
+tela.calculate(80, 0.001)
+tela.run(speed=3)
